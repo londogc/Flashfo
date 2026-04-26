62baa2f32d5757f58ad7673de3b6b0bff98fc20e
londogc <GLENIAM14@GMAIL.COM>
2026-04-25 20:08:45 -0700

    fix: wrap Dashboard in Shell

--- app/page.js
+++ app/page.js
@@ -1,2 +1,6 @@
+import Shell from '@/components/Shell'
 import Dashboard from '@/components/dashboard/Dashboard'
-export default function Home() { return <Dashboard /> }
+
+export default function Home() {
+  return <Shell><Dashboard /></Shell>
+}
