e2f987e813fa090236085523e72b5ecfaa20e5b6
londogc <GLENIAM14@GMAIL.COM>
2026-04-27 22:16:45 -0700

    v6: remove duplicate app/dashboard/page.js

--- app/dashboard/page.js
+++
@@ -1,6 +0,0 @@
-import Shell from '@/components/Shell'
-import Dashboard from '@/components/dashboard/Dashboard'
-
-export default function DashboardPage() {
-  return <Shell><Dashboard /></Shell>
-}
