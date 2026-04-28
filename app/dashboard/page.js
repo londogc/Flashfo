d998fd1169ec1e7b2ebfba6b4df80cf0bfaf2d1b
londogc <GLENIAM14@GMAIL.COM>
2026-04-27 22:01:37 -0700

    v6: add /dashboard route page

---
+++ app/dashboard/page.js
@@ -0,0 +1,6 @@
+import Shell from '@/components/Shell'
+import Dashboard from '@/components/dashboard/Dashboard'
+
+export default function DashboardPage() {
+  return <Shell><Dashboard /></Shell>
+}
